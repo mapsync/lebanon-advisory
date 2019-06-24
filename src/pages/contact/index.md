3630b081f97d2f5778f4eba5f48e9afdd57d349d
unknown <caseyy@mapsync.com>
2019-06-24 12:58:01 -0400

    initial commit

---
+++ src/pages/contact/index.md
@@ -0,0 +1,31 @@
+---
+templateKey: contact-page
+name: Paintsville Utilities
+address: 137 Main St.
+mailing_address: P.O. BOX 630
+city_state_zip: 'Paintsville, KY 41240'
+phone: 606-789-2630
+after_hours_phone: 606-297-2636
+fax: 606-789-2631
+email: mail@paintsvilleutilities.com
+hours: 'Monday - Friday, 7:30 a.m. - 4:00 p.m.'
+website: http://www.paintsvilleutilities.com
+facebook: https://www.facebook.com/Paintsville-Utilities-1092775934154310
+---
+Bill Mike Runyon, Mayor
+
+Tim Spencer, Commissioner
+
+L.B. Lemaster, Commissioner
+
+Mitch Kinner, Commissioner
+
+Jimmy Wright, Commissioner
+
+Patricia Nelson, Commissioner
+
+Bob Pack, General Manager  
+Email: <a href="mailto:shunter@russellsprings.net">bpack@paintsvilleutilities.com</a>
+
+Commission Meeting: Held the 1st Monday of each month
+at 5 pm in the utility office conference room.
---
+++ src/templates/contact-page.js
@@ -0,0 +1,123 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import * as Icon from 'react-feather';
+import Content, { HTMLContent } from '../components/Content'
+
+export const ContactPageTemplate = ({ content, contentComponent, name, address, mailing_address, city_state_zip, phone, after_hours_phone, fax, email, hours }) => {
+  const PageContent = contentComponent || Content
+  return (
+    <div>
+      <div className="container container-main grid-md">
+        <div className="card">
+          <div className="card-header">
+            Address
+          </div>
+          <div className="card-body">
+            {name}<br />
+            {address}<br />
+            {city_state_zip}
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-header">
+            Mailing Address
+          </div>
+          <div className="card-body">
+            {name}<br />
+            {mailing_address}<br />
+            {city_state_zip}
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-header">
+            Phone
+          </div>
+          <div className="card-body">
+            T: <a href={"tel:1-" + phone}>{phone}</a><br />
+            F: {fax}<br />
+            After Hours Gas, Water, or Sewer Emergency: <a href={"tel:1-" + after_hours_phone}>{after_hours_phone}</a><br />
+            E: <a href={"mailto:" + email}>{email}</a>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-header">
+            Hours
+          </div>
+          <div className="card-body">
+            {hours}
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-header">
+            Information
+          </div>
+          <div className="card-body">
+            <PageContent className="content" content={content} />
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        <div className="d-inline-block float-right">
+          <a className="btn btn-sm btn-link tooltip tooltip-left" data-tooltip="Settings" rel="noopener noreferrer" href="https://lebanonky.geosync.cloud/admin" target="_blank">
+            <Icon.Settings size={16}/>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+ContactPageTemplate.propTypes = {
+  address: PropTypes.string,
+  phone: PropTypes.string,
+}
+
+const ContactPage = ({ data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <Layout>
+      <ContactPageTemplate
+        name={post.frontmatter.name}
+        address={post.frontmatter.address}
+        mailing_address={post.frontmatter.mailing_address}
+        city_state_zip={post.frontmatter.city_state_zip}
+        phone={post.frontmatter.phone}
+        after_hours_phone={post.frontmatter.after_hours_phone}
+        fax={post.frontmatter.fax}
+        email={post.frontmatter.email}
+        hours={post.frontmatter.hours}
+        websites={post.frontmatter.websites}
+        contentComponent={HTMLContent}
+        content={post.html}
+      />
+    </Layout>
+  )
+}
+
+ContactPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default ContactPage
+
+export const contactPageQuery = graphql`
+  query ContactPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        name,
+        address,
+        mailing_address,
+        city_state_zip,
+        phone,
+        after_hours_phone,
+        fax,
+        email,
+        hours
+      }
+    }
+  }
+`
